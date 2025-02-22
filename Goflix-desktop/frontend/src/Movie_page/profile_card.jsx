bef7e32f790ba499091f0c225904d591725f6404
Iruos8805 <souris8805@gmail.com>
2025-02-22 13:19:20 +0530

    Created Version1 Horiontal scroll

--- Goflix-desktop/frontend/src/Movie_page/profile_card.jsx
+++ Goflix-desktop/frontend/src/Movie_page/profile_card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProfileCard = ({ imageUrl, name1, name2 }) => {
+const ProfileCard = ({ imageUrl, name, role }) => {
   return (
     <div className="flex flex-col items-center p-4">
       <img
@@ -8,10 +8,11 @@ const ProfileCard = ({ imageUrl, name1, name2 }) => {
         alt="Profile"
         className="w-24 h-24 rounded-full border-2 border-gray-300"
       />
-      <p className="mt-2 text-lg font-semibold">{name1}</p>
-      <p className="text-gray-500">{name2}</p>
+      <p className="mt-2 text-lg font-semibold">{name}</p>
+      <p className="text-gray-500">{role}</p>
     </div>
   );
 };
 
 export default ProfileCard;
+  
