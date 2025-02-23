5589760acc86d8a6efc65ecb35320220d6a4e0bd
Vishnu Tejas <vishnutejase@gmail.com>
2025-02-23 17:22:11 +0530

    GO: fmt

--- Goflix-desktop/main.go
+++ Goflix-desktop/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"embed"
 
+	"Goflix-Desktop/backend/stream"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
-	"Goflix-Desktop/backend/stream"
 )
 
 //go:embed all:frontend/dist
@@ -37,4 +37,3 @@ func main() {
 
 	stream.InitStreamApp()
 }
-
